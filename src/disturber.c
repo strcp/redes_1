5a53a336d129cd63e07732be430f5882973a79a0
Cristiano Fernandes <cristianobfernandes@gmail.com>
2010-11-03 11:34:09 -0200

    Code style.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

--- src/disturber.c
+++ src/disturber.c
@@ -126,6 +126,7 @@ void init_cvictim() {
 
 void debug_cvivtim(struct victim *cli) {
 	char buf[INET6_ADDRSTRLEN];
+
 	printf("HWADDR: %s\n", ether_ntoa(&(cli->hwaddr)));
 	memset(buf, 0, INET6_ADDRSTRLEN);
 	inet_ntop(AF_INET6, cli->ipv6.s6_addr, buf, INET6_ADDRSTRLEN);
@@ -135,24 +136,31 @@ void debug_cvivtim(struct victim *cli) {
 struct cli_victim *get_cvictim(struct ethhdr *eth) {
 	struct cli_victim *cli;
 	struct ip6_hdr *ip6;
+
 	ip6 = (struct ip6_hdr *)((char *)eth + sizeof(struct ethhdr));
+
 	for (cli = cvictim; cli; cli = cli->nxt) {
 		printf("entrei");
 		if (memcmp(&(cli->cv_victim.hwaddr), &(eth->h_source),
 							sizeof(struct ether_addr)) == 0) {
 			printf("Cliente existente\n");
+
 			return cli;
 		}
 	}
+
 	printf("Cliente inexistente\n");
+
 	cli = cvictim;
 	cvictim = (struct cli_victim *)malloc(sizeof(struct cli_victim));
 	cvictim->nxt = cli;
+
 	memcpy(&(cvictim->cv_victim.hwaddr), &(eth->h_source), ETH_ALEN);
 	//cvictim->cv_victim.ipv4
 	memcpy(&(cvictim->cv_victim.ipv6), &(ip6->ip6_src), INET6_ADDRSTRLEN);
 	cvictim->cv_victim.poisoned = 0;
 	//cvictim->th = (struct pthread_t *)malloc(sizeof(struct pthread_t));
+
 	return cvictim;
 }
 
