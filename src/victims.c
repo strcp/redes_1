fcfa3472b3ca05d354a79e1f0f7ff80856152a54
Cristiano Fernandes <cristianobfernandes@gmail.com>
2010-11-13 02:49:55 -0200

    Cleanup.
    Signed-off-by: Cristiano Fernandes <cristianobfernandes@gmail.com>

--- src/victims.c
+++ src/victims.c
@@ -62,69 +62,6 @@ void populate_cvictim(char *pkt) {
 	debug_cvivtim(cvictim);
 }
 
-/*struct victim *get_cvictim(struct ether_addr *hwaddr) {
-	struct cli_victim *cli;
-
-	for (cli = cvictim; cli; cli = cli->nxt) {
-		printf("entrei");
-		if (memcmp(&(cli->cv_victim->hwaddr), hwaddr,
-							sizeof(struct ether_addr)) == 0) {
-			printf("Cliente existente\n");
-
-			return cli->cv_victim;
-		}
-	}
-
-	return NULL;
-}
-
-int add_cvictm(struct ethhdr *eth) {
-	struct ip6_hdr *ip6;
-	struct cli_victim *cli;
-
-	ip6 = (struct ip6_hdr *)((char *)eth + sizeof(struct ethhdr));
-
-	cli = cvictim;
-	cvictim = (struct cli_victim *)malloc(sizeof(struct cli_victim));
-	cvictim->nxt = cli;
-
-	cvictim->cv_victim = malloc(sizeof(struct victim));
-	memcpy(&(cvictim->cv_victim->hwaddr), &(eth->h_source), ETH_ALEN);
-	memcpy(&(cvictim->cv_victim->ipv6), &(ip6->ip6_src), INET6_ADDRSTRLEN);
-	cvictim->cv_victim->poisoned = 0;
-	//cvictim->th = (struct pthread_t *)malloc(sizeof(struct pthread_t));
-
-	return 1;
-}
-
-void del_cvictm(struct victim *vic) {
-	struct cli_victim *cli, *aux;
-
-	aux = NULL;
-	for (cli = cvictim; cli; cli = cli->nxt) {
-		if (memcmp(&(cli->cv_victim->hwaddr), &(vic->hwaddr),
-					sizeof(struct ether_addr)) == 0) {
-			if (aux == NULL) {
-				cvictim = cli->nxt;
-				free(cvictim);
-				return;
-			} else {
-				aux->nxt = cli->nxt;
-				free(cli);
-				return;
-			}
-		}
-		aux = cli;
-	}
-}
-
-void *poison_vclient(void *conn) {
-	//poisoning
-	printf("oiaeu\n");
-
-	pthread_exit((void*)EXIT_SUCCESS);
-}
-*/
 void init_svictim(const char *sv_address) {
 	char server_victim[INET6_ADDRSTRLEN];
 	char *pkt;
@@ -143,22 +80,8 @@ void init_svictim(const char *sv_address) {
 
 	if (pkt)
 		free(pkt);
-
-
 }
 
 void init_cvictim() {
 	cvictim = NULL;
 }
-/*
-void cleanup_cvictims() {
-	struct cli_victim *cli, *aux;
-
-	aux = NULL;
-
-	for (cli = cvictim, aux = cli->nxt; cli; cli = aux) {
-		free(cli);
-	}
-
-	cvictim = NULL;
-}*/
